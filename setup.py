eab50f9bb07a5b87713cd7a53bba90d1a4044196
Arjun <86394506+Arjun-M@users.noreply.github.com>
2022-03-21 20:36:17 +0530

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+
+setup(
+      name="pyTelegramClient",
+      version="0.0.1",
+      description="Open Source, Light Weight, Powerful Telegram Bot Package .",
+      author="Arjun-M",      
+      url="https://github.com/Arjun-M/pyTelegramClient",
+      packages = ['pyTelegramClient'] ,
+      license='MIT',      
+      install_requires=['requests'],
+)
