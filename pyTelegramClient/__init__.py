4be2e35959f4a999d68bf3da18599b332bb44fcb
Arjun <86394506+Arjun-M@users.noreply.github.com>
2022-04-09 10:42:34 +0530

    Update __init__.py

--- pyTelegramClient/__init__.py
+++ pyTelegramClient/__init__.py
@@ -1,4 +1,4 @@
 from .client import Client              # Router and other tools 
 from .telegram import Telegram          # Telegram bot API methods
 from .broadcaster import Broadcaster    # in-built Broadcaster
-from .client import Button              # Keyboard Markup tool
+from .types import Button , Markup      # Button parser .
